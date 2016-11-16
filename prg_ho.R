21b56552fa4a7bd270ea333739c7d7d608546f9d
yaojenkuo <tonykuoyj@gmail.com>
2016-11-17 00:50:15 +0900

    first commit

---
+++ prg_ho.R
@@ -0,0 +1,35 @@
+# A function that is able to generate n integers between min and max
+random.int.generate <- function(n = 6, min = -10, max = 10){
+  min <- as.integer(min)
+  max <- as.integer(max)
+  random_vector <- min:max
+  random_int <- sample(random_vector, size = n, replace = FALSE)
+  return(random_int)
+}
+
+# A function that is able to generate the result vector of N + M for the x_vector
+generate.result.vector <- function() {
+  x_vector <- low_threshold:high_threshold
+  x_j_vector <- rep(NA, times = length(random_int))
+  N_plus_M_vector <- rep(NA, times = length(x_vector))
+  for (j in 1:length(x_vector)) {
+    x <- x_vector[j]
+    for (i in 1:length(random_int)) {
+      x_j_vector[i] <- x - sum(random_int[1:i])
+    }
+    x_j_N <- sum(x_j_vector >= high_threshold)
+    x_j_M <- sum(x_j_vector <= low_threshold)
+    x_j_N_plus_M <- x_j_N + x_j_M
+    N_plus_M_vector[j] <- x_j_N_plus_M
+  }
+  return(N_plus_M_vector)
+}
+
+# Assign inputs
+low_threshold <- 0L
+high_threshold <- 25L
+random_int <- random.int.generate()
+N_plus_M_vector <- generate.result.vector()
+
+# Find out X
+answers <- which(N_plus_M_vector == min(N_plus_M_vector)) - 1
